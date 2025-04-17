117ada67dd72086a0dc21d49612bdfbbedd09202
veden <vedenin1980@gmail.com>
2025-04-17 12:45:02 +0200

    init

--- src/main/java/org/telran/lecture_03_recursion/test.js
+++ src/main/java/org/telran/lecture_03_recursion/test.js
@@ -1,32 +1,40 @@
-function factorial(number) {
-    if (number === 0) {
-        return 1;
-    } else {
-        return number*factorial(number-1);
-    }
-}
+
 function factorial5(number) {
-    return number*factorial4(number-1);
+    let value = factorial4(number-1);
+    return number * value;
 }
 
 function factorial4(number) {
-    return number*factorial3(number-1);
+    let value = factorial3(number-1);
+    return number * value;
 }
 
 function factorial3(number) {
-    return number*factorial2(number-1);
+    let value = factorial2(number-1);
+    return number * value;
 }
 
 function factorial2(number) {
-    return number*factorial1(number-1);
+    let value = factorial1(number-1);
+    return number * value;
 }
 
 function factorial1(number) {
-    return number*factorial0(number-1);
+    let value = factorial0(number-1);
+    return number * value;
 }
 
 function factorial0(number) {
     return 1;
+}
+
+function factorial(number) {
+    if (number === 0) {
+        return 1;
+    } else {
+        let value = factorial(number-1);
+        return number * value;
+    }
 }
 
 console.log("factorial " + factorial5(5));
